85fed061e999c6a1b565a984568f116f8771f057
Carl Ansell <10620927+carlansell94@users.noreply.github.com>
2024-08-10 13:31:53 +0100

    feat: Update editor preview to match front-end format

--- src/index.js
+++ src/index.js
@@ -1,100 +1,130 @@
 const { registerBlockType } = wp.blocks;
-const { PanelBody, ToggleControl, Disabled } = wp.components;
+const { PanelBody, ToggleControl } = wp.components;
 const { InspectorControls, useBlockProps } = wp.blockEditor;
 const { useSelect } = wp.data;
 const { __ } = wp.i18n;
 
+import './editor.scss';
 import './style.scss';
 import metadata from './block.json';
 
+const Edit = ( { attributes, setAttributes } ) => {
+	const { showPostCounts, showTitle } = attributes;
+	const allPosts = useSelect(
+		( select ) =>
+			select( 'core' ).getEntityRecords( 'postType', 'post', {
+				per_page: -1,
+			} ),
+		[]
+	);
+
+	const getGroupedPosts = ( posts ) => {
+		if ( ! posts ) return [];
+
+		const groupedPosts = posts.reduce( ( acc, post ) => {
+			const date = new Date( post.date );
+			const year = date.getFullYear();
+			const month = date.toLocaleString( 'default', {
+				month: 'long',
+			} );
+
+			let yearGroup = acc.find( ( group ) => group.year === year );
+			if ( ! yearGroup ) {
+				yearGroup = { year, months: [] };
+				acc.push( yearGroup );
+			}
+
+			let monthGroup = yearGroup.months.find(
+				( m ) => m.month === month
+			);
+			if ( ! monthGroup ) {
+				monthGroup = { month, posts: [] };
+				yearGroup.months.push( monthGroup );
+			}
+
+			monthGroup.posts.push( post );
+			return acc;
+		}, [] );
+
+		groupedPosts.sort( ( a, b ) => b.year - a.year );
+
+		groupedPosts.forEach( ( yearGroup ) => {
+			yearGroup.months.sort(
+				( a, b ) =>
+					new Date( `01 ${ b.month } 0` ) -
+					new Date( `01 ${ a.month } 0` )
+			);
+		} );
+
+		return groupedPosts;
+	};
+
+	const groupedPosts = getGroupedPosts( allPosts );
+
+	return (
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Settings' ) }>
+					<ToggleControl
+						label={ __( 'Show title' ) }
+						checked={ showTitle }
+						onChange={ () =>
+							setAttributes( {
+								showTitle: ! showTitle,
+							} )
+						}
+					/>
+					<ToggleControl
+						label={ __( 'Show post counts' ) }
+						checked={ showPostCounts }
+						onChange={ () =>
+							setAttributes( {
+								showPostCounts: ! showPostCounts,
+							} )
+						}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<div { ...useBlockProps() }>
+				{ showTitle && <h3>Archive</h3> }
+				{ groupedPosts.map( ( yearGroup ) => (
+					<details
+						className="wp-block-carlansell94-summary-archive-year"
+						key={ yearGroup.year }
+					>
+						<summary>{ yearGroup.year }</summary>
+						{ yearGroup.months.map( ( monthGroup ) => (
+							<details
+								className="wp-block-carlansell94-summary-archive-month"
+								key={ monthGroup.month }
+							>
+								<summary>
+									{ monthGroup.month }
+									{ showPostCounts &&
+										` (${ monthGroup.posts.length })` }
+								</summary>
+								<ul>
+									{ monthGroup.posts.map( ( post ) => (
+										<li key={ post.id }>
+											<span
+												dangerouslySetInnerHTML={ {
+													__html: post.title.rendered,
+												} }
+											/>
+										</li>
+									) ) }
+								</ul>
+							</details>
+						) ) }
+					</details>
+				) ) }
+			</div>
+		</>
+	);
+};
+
 registerBlockType( metadata.name, {
-	edit( { attributes, setAttributes } ) {
-		const { showPostCounts, showTitle } = attributes;
-		const posts = useSelect(select =>
-            select('core').getEntityRecords('postType', 'post', { per_page: -1 })
-        , []);
-
-		const groupPostsByYearAndMonth = (posts) => {
-            if (!posts) return [];
-
-            const grouped = posts.reduce((acc, post) => {
-				const date = new Date(post.date);
-				const year = date.getFullYear();
-				const month = date.toLocaleString('default', { month: 'long' });
-		
-				let yearGroup = acc.find(group => group.year === year);
-				if (!yearGroup) {
-					yearGroup = { year, months: [] };
-					acc.push(yearGroup);
-				}
-		
-				let monthGroup = yearGroup.months.find(m => m.month === month);
-				if (!monthGroup) {
-					monthGroup = { month, posts: [] };
-					yearGroup.months.push(monthGroup);
-				}
-		
-				monthGroup.posts.push(post);
-				return acc;
-			}, []);
-		
-			grouped.sort((a, b) => b.year - a.year);
-		
-			grouped.forEach(yearGroup => {
-				yearGroup.months.sort((a, b) => new Date(`01 ${b.month} 0`) - new Date(`01 ${a.month} 0`));
-			});
-		
-			return grouped;
-        };
-
-		const groupedPosts = groupPostsByYearAndMonth(posts);
-	
-		return (
-			<>
-				<InspectorControls>
-					<PanelBody title={ __( 'Settings' ) }>
-						<ToggleControl
-							label={ __( 'Show title' ) }
-							checked={ showTitle }
-							onChange={ () =>
-								setAttributes( {
-									showTitle: ! showTitle,
-								} )
-							}
-						/>
-						<ToggleControl
-							label={ __( 'Show post counts' ) }
-							checked={ showPostCounts }
-							onChange={ () =>
-								setAttributes( {
-									showPostCounts: ! showPostCounts,
-								} )
-							}
-						/>
-					</PanelBody>
-				</InspectorControls>
-				<div {...useBlockProps()}>
-				{groupedPosts.map(yearGroup => (
-    <details key={yearGroup.year}>
-        <summary>{yearGroup.year}</summary>
-        {yearGroup.months.map(monthGroup => (
-            <details key={monthGroup.month}>
-                <summary>{monthGroup.month}</summary>
-                <ul>
-                    {monthGroup.posts.map(post => (
-                        <li key={post.id}>
-                            {post.title.rendered}
-                        </li>
-                    ))}
-                </ul>
-            </details>
-        ))}
-    </details>
-))}
-                </div>
-			</>
-		)
-	},
+	edit: Edit,
 	save() {
 		return null;
 	},
